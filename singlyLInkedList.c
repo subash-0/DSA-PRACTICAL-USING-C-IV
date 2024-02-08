4844dbe371a69b4a4a7886379c50992c43832e7f
subash-0 <subash67335@gmail.com>
2024-02-08 10:01:28 +0545

    lab_report_added

---
+++ singlyLInkedList.c
@@ -0,0 +1,262 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node
+{
+    int info;
+    struct node *next;
+};
+struct node *head = NULL, *ptr, *newptr, *prevptr;
+struct node *getnode()
+{
+    struct node *np;
+    np = (struct node *)malloc(sizeof(struct node));
+    printf("enter the data:");
+    scanf("%d", &np->info);
+    np->next = NULL;
+    return np;
+}
+void create()
+{
+    struct node *last;
+
+    char ch;
+    do
+    {
+        newptr = getnode();
+        if (head == NULL)
+            head = newptr;
+        else
+            last->next = newptr;
+        last = newptr;
+        printf("Do you want to add more..(y/n)");
+        scanf(" %c", &ch);
+    } while (ch == 'y' || ch == 'Y');
+}
+void display()
+{
+    ptr = head;
+    if(ptr==NULL){
+        printf("list is empty");
+    }
+    else{
+    printf("The entered elements are:\n");
+    while (ptr != NULL)
+    {
+        printf("%d\t", ptr->info);
+        ptr = ptr->next;
+    }
+    }
+}
+void insertAtFront()
+{
+    newptr = getnode();
+    newptr->next = head;
+    head = newptr;
+    printf("%d added", head->info);
+}
+void insertAtLast()
+{
+    newptr = getnode();
+    if (head != NULL)
+    {
+
+        ptr = head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = newptr;
+        printf("%d is added", newptr->info);
+    }
+    else
+    {
+        head = newptr;
+        printf("%d is added", head->info);
+    }
+}
+void insertAnywhere()
+{
+    int key;
+    if(head!=NULL){
+    printf("enter key");
+    scanf("%d", &key);
+    ptr = head;
+    while (ptr != NULL && ptr->info != key)
+    {
+        ptr = ptr->next;
+    }
+    if (ptr == NULL)
+    {
+        printf("node with key does not exist");
+    }
+    else
+    {
+        newptr = getnode();
+        newptr->next = ptr->next;
+        ptr->next = newptr;
+    }
+    printf("%d added!", newptr->info);
+
+    }else{
+        printf("list is empty");
+    }
+}
+void insert()
+{
+    int ch;
+    while (1)
+    {
+        system("cls");
+        printf("1.Insert at front\n2.Insert at last\n3.Insert anywhere\nAny Key to Goback\n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            insertAtFront();
+            break;
+        case 2:
+            insertAtLast();
+            break;
+        case 3:
+            insertAnywhere();
+            break;
+        default:
+            printf("press Enter..");
+            return;
+        }
+        
+    }
+}
+void deleteFromFront()
+{
+    if (head == NULL)
+    {
+        printf("linked list is empty!");
+    }
+    else
+    {
+        ptr = head;
+        printf("%d is deleted", head->info);
+        head = head->next;
+        free(ptr);
+    }
+}
+void deleteFromLast()
+{
+    prevptr = NULL;
+    if (head == NULL)
+    {
+        printf("linked list is empty");
+    }
+    else
+    {
+        ptr = head;
+        while (ptr->next != NULL)
+        {
+            prevptr = ptr;
+            ptr = ptr->next;
+        }
+        printf("%d is deleted", ptr->info);
+        if (!prevptr)
+        {
+            head = NULL;
+        }
+        else
+        {
+
+            prevptr->next = NULL;
+        }
+        free(ptr);
+    }
+}
+void deleteFromAnywhere()
+{
+    int key;
+    if (head == NULL)
+    {
+        printf("linked list is empty");
+    }
+    else
+    {
+        printf("enter key\n");
+        scanf("%d", &key);
+        ptr = head;
+        while (ptr != NULL && ptr->info != key)
+        {
+            prevptr = ptr;
+            ptr = ptr->next;
+        }
+        if (ptr == NULL)
+        {
+            printf("node does not exist");
+        }
+        else
+        {
+            printf("%d is deleted:", ptr->info);
+            if (prevptr)
+                prevptr->next = ptr->next;
+            else
+                head = ptr->next;
+            free(ptr);
+        }
+    }
+}
+
+void delete()
+{
+    int ch;
+    while (1)
+    {
+        system("cls");
+        printf("1.delete at front\n2.delete at last\n3.delete anywhere\nAny Key to Goback\n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            deleteFromFront();
+            break;
+        case 2:
+            deleteFromLast();
+            break;
+        case 3:
+            deleteFromAnywhere();
+            break;
+        default:
+            printf("press Enter..");
+            return;
+        }
+        
+    }
+}
+int main()
+{
+    int choice;
+    create();
+
+    while (1)
+    {
+        system("cls");
+        printf("1.Insert\n2.Delete\n3.display\n4.exit\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            insert();
+            break;
+        case 2:
+            delete ();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+            break;
+        }
+        
+    }
+
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,299 @@
-getc
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int info;
+    struct node *next;
+    struct node *prev;
+};
+struct node *head=NULL,*ptr,*newptr;
+
+struct node* getnode()
+{
+    struct node* np;
+    np=(struct node*)malloc(sizeof(struct node));
+    printf("Enter the data:");
+    scanf("%d",&np->info);
+    np->next =NULL;
+    np->prev = NULL;
+    return np;
+};
+
+void createList(){
+    char ch;
+    do {
+        newptr = getnode();
+    if (head == NULL) {
+        head = newptr;
+    }else {
+        ptr->next = newptr;
+        newptr->prev = ptr;
+     }
+    ptr = newptr;
+    printf("Would you like to add more data:(Y/N)");
+    scanf(" %c",&ch);
+    }while (ch == 'y' || ch=='Y');
+}
+void display(){
+    ptr = head;
+    printf("Data in Doubly Link List are:\n");
+    while (ptr!=NULL) {
+        printf("%d\t",ptr->info);
+        ptr=ptr->next;
+    
+    }
+
+}
+void insertAtFront()
+{
+    newptr = getnode();
+    if (head != NULL)
+    {
+        newptr->next = head;
+        head->prev = newptr;
+    }
+    printf("%d is inserted", newptr->info);
+    head = newptr;
+}
+void insertAtLast()
+{
+    newptr = getnode();
+    if (head != NULL)
+    {
+        ptr = head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = newptr;
+        newptr->prev = ptr;
+    }
+    else
+    {
+        head = newptr;
+    }
+    printf("%d is inserted", newptr->info);
+}
+void insertAnywhere()
+{
+    int key;
+    if (head != NULL)
+    {
+        newptr = getnode();
+        ptr = head;
+        printf("enter key:");
+        scanf("%d", &key);
+        while (ptr != NULL && ptr->info != key)
+        {
+            ptr = ptr->next;
+        }
+        if (ptr == NULL)
+        {
+            printf("\nnode with key does not exist");
+        }
+        else
+        {
+
+            newptr->next = ptr->next;
+            ptr->next = newptr;
+            ptr->next->prev = newptr;
+            newptr->prev = ptr;
+            printf("%d is inserted", newptr->info);
+        }
+    }
+    else
+    {
+        insertAtFront();
+    }
+}
+void insert()
+{
+    int ch;
+    if (head != NULL)
+    { 
+       
+        do{
+            system("cls");
+            printf("1.INSERT at FRONT\n2.INSERT  at LAST \n3.INSERT ANYWHERE \n4. PRESS ENTER TO EXIT\n SELECT FROM OPTION :");
+            scanf("%d", &ch);
+            switch (ch)
+            {
+            case 1:
+                insertAtFront();
+                break;
+            case 2:
+                insertAtLast();
+                break;
+            case 3:
+                insertAnywhere();
+                break;
+            default:
+            printf("press enter");
+                return;
+            }
+          
+        } while (ch!=4);
+    }
+    else
+    {
+        insertAtFront();
+    }
+}
+
+void deleteFromFront()
+{
+    if (head == NULL)
+    {
+        printf("linked list is empty!!");
+    }
+    else
+    {
+        ptr = head;
+        if (head->next != NULL)
+        {
+            head = head->next;
+            head->prev = NULL;
+        }
+        else
+        {
+            head = NULL;
+        }
+        printf("%d is deleted", ptr->info);
+        free(ptr);
+    }
+}
+void deleteFromLast()
+{
+    if (head == NULL)
+    {
+        printf("linked list is empty!!");
+    }
+    else
+    {
+        ptr = head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        printf("%d is deleted", ptr->info);
+        if (ptr->prev != NULL)
+        {
+            ptr->prev->next = NULL;
+        }
+        else
+        {
+            head = NULL;
+        }
+        free(ptr);
+    }
+}
+void deleteFromAnywhere()
+{
+    int key;
+    if (head == NULL)
+    {
+        printf("linked list is empty");
+    }
+    else
+    {
+        printf("enter key");
+        scanf("%d", &key);
+        ptr = head;
+        while (ptr != NULL && ptr->info != key)
+        {
+            ptr = ptr->next;
+        }
+        if (ptr == NULL)
+        {
+            printf("data with %d does not exist", key);
+        }
+        else
+        {
+            if (ptr->prev != NULL)
+            {
+                if (ptr->next != NULL)
+                {
+                    ptr->prev->next = ptr->next;
+                    ptr->next->prev = ptr->prev;
+                }
+                else
+                {
+                    ptr->prev->next = NULL;
+                }
+            }
+            else
+            {
+                if (ptr->next)
+                {
+                    ptr->next->prev = NULL;
+                    head = ptr->next;
+                }
+                else
+                {
+                    head = NULL;
+                }
+            }
+            printf("%d is deleted", ptr->info);
+            free(ptr);
+        }
+    }
+}
+void delete()
+{
+    int ch;
+    if (head != NULL)
+    {
+        
+        while (1) {
+            system("cls");
+            printf("1.DELETE at FRONT\n2.DELETE at LAST\n3.DELTE ANYWHERE\nPRESS ENTER TO EXIT\n Select your option:");
+            scanf("%d", &ch);
+            switch (ch)
+            {
+            case 1:
+                deleteFromFront();
+                break;
+            case 2:
+                deleteFromLast();
+                break;
+            case 3:
+                deleteFromAnywhere();
+                break;
+            default:
+            return;
+            }
+           
+        }
+    }
+    else
+    {
+        printf("doubly linked list is empty!!");
+    }
+}
+int main(){
+        int c;
+     createList();
+     
+     do {
+     printf("\n1.INSERT \t 2. DISPLAY \t 3. DELETE \t 4.EXIT \n Enter your choice:");
+     scanf("%d",&c);
+     switch (c) {
+        case 1:
+        insert();
+        break;
+        case 2:
+        display();
+        break;
+        case 3:
+        printf("Deletion will be provided soon!");
+        break;
+        case 4:
+        exit(0);
+        break;
+        default:
+        printf("No such option is available.");
+     
+     }
+     }while (c!=4);
+    return 0;
+}
