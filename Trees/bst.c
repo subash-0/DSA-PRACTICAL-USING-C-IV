605e252c52e00f21dc9d23a194463a237f51b9d3
subash-0 <subash67335@gmail.com>
2024-02-16 09:45:52 +0545

    insert_key is implemented

--- Trees/bst.c
+++ Trees/bst.c
@@ -8,9 +8,8 @@ struct node {
 };  
 struct node * root=NULL,*temp;
 
-struct node* get_new_node(){  
+struct node* get_new_node(int x){  
   temp = malloc(sizeof(struct node));  
-  int x;
   printf("Enter data:");
   scanf("%d",&x);
   temp->data= x;
@@ -20,6 +19,20 @@ struct node* get_new_node(){
   return temp;
 }  
 
+struct node *insert_key(struct node *root, int value) {
+    if (root == NULL) {
+        return get_new_node(value);
+    }
+    if (value < root->data) {
+        root->left = insert_key(root->left, value);
+        printf("inserted left of %d\n", root->data);
+    }
+    else if (value > root->data) {
+        root->right = insert_key(root->right, value);
+        printf("inserted right of %d\n", root->data);
+    }
+    return root;
+}
   
 int main() {  
 
